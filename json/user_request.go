fd52d7a3e8271fb02ae8b091ab4d4b35df5f5ede
Laion Corcino <laioncorcino@gmail.com>
2023-02-28 03:04:42 -0300

    [feat] convertendo request to domain e domain to response

--- json/user_request.go
+++ json/user_request.go
@@ -1,8 +1,21 @@
 package json
 
+import (
+	"github.com/laioncorcino/go-user/domain/model"
+)
+
 type UserRequest struct {
 	Name     string `json:"name" binding:"required,min=3,max=100"`
 	Age      int8   `json:"age" binding:"required,min=2,max=140"`
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=6,max=50"`
+}
+
+func (request *UserRequest) ToModel() model.UserDomain {
+	var domain model.UserDomain
+	domain.SetName(request.Name)
+	domain.SetEmail(request.Email)
+	domain.SetAge(request.Age)
+	domain.SetPassword(request.Password)
+	return domain
 }
--- json/user_response.go
+++ json/user_response.go
@@ -1,8 +1,19 @@
 package json
 
+import "github.com/laioncorcino/go-user/domain/model"
+
 type UserResponse struct {
 	ID    string `json:"id"`
 	Name  string `json:"name"`
 	Age   int8   `json:"age"`
 	Email string `json:"email"`
+}
+
+func ToResponse(userDomain model.UserDomain) UserResponse {
+	return UserResponse{
+		userDomain.GetID(),
+		userDomain.GetName(),
+		userDomain.GetAge(),
+		userDomain.GetEmail(),
+	}
 }
